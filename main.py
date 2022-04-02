02900040df01f85303118ba66a48d2d3413abc5b
bnagy <balazs.nagy@kpmg.hu>
2022-04-02 14:11:45 +0200

    updated main script

--- main.py
+++ main.py
@@ -2,19 +2,18 @@ import pandas as pd
 from src.scraper import ElteMathThesisParser
 
 
-# initialize object
-elte_ttk_math = ElteMathThesisParser()
-# link to homepage
-print(elte_ttk_math.home)
+def run():
+    # initialize object
+    elte_ttk_math = ElteMathThesisParser()
+    # print the link to homepage
+    print(elte_ttk_math.home)
 
-# available programs as dataframe
-programs = pd.DataFrame.from_dict(elte_ttk_math.programs,
-                                  orient='index',
-                                  columns=['link']).reset_index().rename({'index': 'program'}, axis=1)
-# parse all thesis info to single dataframe
-data = elte_ttk_math.parse_all()
+    # parse all thesis info to single dataframe
+    data = elte_ttk_math.parse_all()
 
-# save programs and thesis data to Excel
-with pd.ExcelWriter('elte-ttk-thesis-list.xlsx') as writer:
-    programs.to_excel(writer, sheet_name='programs', index=False)
-    data.to_excel(writer, sheet_name='data', index=False)
+    # save parsed info to excel
+    elte_ttk_math.to_excel(data=data, save_to='elte-ttk-thesis-list.xlsx')
+
+
+if __name__ == '__main__':
+    run()
