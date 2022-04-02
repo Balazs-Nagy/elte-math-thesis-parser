20b727adcf693813dc0a56e97edbd82ab2b468e8
bnagy <balazs.nagy@kpmg.hu>
2022-04-02 23:23:35 +0200

    updated main.py script

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ def run():
     # print the link to homepage
     print(elte_ttk_math.home)
     # parse thesis info to single dataframe for the given programs
-    data = elte_ttk_math.parse_all(programs=['MSc Biztosítási és pénzügyi matematikus'])
+    data = elte_ttk_math.parse_all(programs=None)
     # save parsed info to excel
     elte_ttk_math.to_excel(data=data, save_to='elte-ttk-thesis-list.xlsx')
+    # download theses corresponding to selected programs
+    programs = ['BSc Matematikus diplomamunkák', 'MSc Biztosítási és pénzügyi matematikus']
     # download theses (pdf files)
-    elte_ttk_math.download(data, download_delay=5)
+    elte_ttk_math.download(data[data.program.isin(programs)], download_delay=3)
 
 
 if __name__ == '__main__':
